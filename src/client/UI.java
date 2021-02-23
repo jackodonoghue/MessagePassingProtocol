ae298c0001a56e50652fe04ab1cced6441302089
Jack ODonoghue <jacklodonoghue@gmail.com>
2021-02-23 16:53:10 +0000

    created project. proposed design for UI

---
+++ src/client/UI.java
@@ -0,0 +1,24 @@
+package client;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class UI {
+    public static void main(String[] args) {
+        //Create message sending panel
+
+        //add type message textbox
+
+        //add send message button
+
+        //add download message panel
+
+        //add button to download messages
+
+        //add panel to fill with messages -- is there a specific text panel?
+
+        //create jframe
+        //set size
+        //set visible
+    }
+}
